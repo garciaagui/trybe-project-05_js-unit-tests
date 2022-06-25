2528fe29e3c61d07e33cdaddd17a0e3992e485e8
Guilherme Garcia <garciaguig@gmail.com>
2022-06-25 15:56:33 -0300

    Requisito 03 - Code Refactoring

--- src/vqv.js
+++ src/vqv.js
@@ -19,13 +19,11 @@
 */
 
 const vqv = (name, age) => {
-  if (!name || !age) { return undefined; }
-
+  if (!name || !age) return undefined;
   const paragraph = `Oi, meu nome é ${name}!
 Tenho ${age} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
-
   return paragraph;
 };
 
